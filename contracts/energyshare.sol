2056f4f0bfbbba8cfa009d189301630a5af59ddb
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2023-11-17 01:54:39 +0530

    minor fixes

--- contracts/energyshare.sol
+++ contracts/energyshare.sol
@@ -3,142 +3,127 @@ pragma solidity ^0.8.0;
 
 contract EnergySharing {
   uint private newSellOfferId = 1;
-  uint private newBuyOfferId = 1;
+    uint private newBuyOfferId = 1;
 
-  struct SellOffer {
-    uint id;
-    address payable seller;
-    uint minEnergyUnits;
-    uint maxEnergyUnits;
-    uint minPrice;
-    uint bestBuyOfferId;
-    uint[] buyOfferIds;
-    bool sold;
-  }
+    struct SellOffer {
+        uint id;
+        address payable seller;
+        uint minEnergyUnits;
+        uint maxEnergyUnits;
+        uint minPrice;
+        uint bestBuyOfferId;
+        uint[] buyOfferIds;
+        bool sold;
+    }
 
-  struct BuyOffer {
-    uint id;
-    address payable buyer;
-    uint sellOfferId;
-    uint numberOfEnergyUnits;
-    uint price;
-  }
+    struct BuyOffer {
+        uint id;
+        address payable buyer;
+        uint sellOfferId;
+        uint numberOfEnergyUnits;
+        uint price;
+    }
 
-  mapping(uint => SellOffer) private sellOffers;
-  mapping(uint => BuyOffer) private buyOffers;
-  mapping(address => uint[]) private sellOfferList;
-  mapping(address => uint[]) private buyOfferList;
+    mapping(uint => SellOffer) private sellOffers;
+    mapping(uint => BuyOffer) private buyOffers;
+    uint[] private sellOfferKeys; // Array to store sell offer keys
+    mapping(address => uint[]) private sellOfferList;
+    mapping(address => uint[]) private buyOfferList;
 
-  // make sure the sale offer exists
-  modifier sellOfferExists(uint _sellOfferId) {
+    modifier sellOfferExists(uint _sellOfferId) {
+        require(_sellOfferId > 0 && _sellOfferId < newSellOfferId, "Sell offer does not exist");
+        _;
+    }
 
-    require(
-      _sellOfferId > 0 && _sellOfferId < newSellOfferId,
-      "Sell offer does not exist"
-    );
-    _;
-  }
+    function createSellOffer(
+        uint _minEnergyUnits,
+        uint _maxEnergyUnits,
+        uint _minPrice
+    ) external {
+        require(_minEnergyUnits > 0, "Minimum energy units must be greater than 0");
+        require(_maxEnergyUnits >= _minEnergyUnits, "Maximum energy units must be greater than or equal to minimum energy units");
+        require(_minPrice > 0, "Minimum price per unit must be greater than 0");
 
-  function createSellOffer(
-    uint _minEnergyUnits,
-    uint _maxEnergyUnits,
-    uint _minPrice
-  ) external {
-    require(_minEnergyUnits > 0, "Minimum energy units must be greater than 0");
-    require(_maxEnergyUnits >= _minEnergyUnits, "Maximum energy units must be greater than or equal to minimum energy units");
-    require(_minPrice > 0, "Minimum price per unit must be greater than 0");
-
-    sellOffers[newSellOfferId] = SellOffer({
+        sellOffers[newSellOfferId] = SellOffer({
             id: newSellOfferId,
             seller: payable(msg.sender),
             minEnergyUnits: _minEnergyUnits,
             maxEnergyUnits: _maxEnergyUnits,
             minPrice: _minPrice,
-            bestBuyOfferId: 0, // Initialize as 0
-            buyOfferIds: new uint[](0), // Initialize as an empty array
+            bestBuyOfferId: 0,
+            buyOfferIds: new uint[](0),
             sold: false
         });
+
+        sellOfferKeys.push(newSellOfferId); // Add the sell offer ID to the keys array
 
         // Add the sell offer ID to the seller's list
         sellOfferList[msg.sender].push(newSellOfferId);
 
         newSellOfferId++; // Increment the sell offer ID for the next offer.
-  }
-
-  function createBuyOffer(uint _sellOfferId, uint _numberOfEnergyUnits, uint _price) public sellOfferExists(_sellOfferId) {
-    SellOffer storage sellOffer = sellOffers[_sellOfferId];
-
-    require(msg.sender != sellOffer.seller, "Buyer cannot be the seller");
-    require(_numberOfEnergyUnits > 0, "Number of energy units must be greater than 0");
-    require(_price > 0, "Price must be greater than 0");
-
-    // Create a new BuyOffer struct
-    uint newBuyOfferId = buyOffers.length;
-    BuyOffer memory buyOffer = BuyOffer({
-        id: newBuyOfferId,
-        buyer: payable(msg.sender),
-        sellOfferId: _sellOfferId,
-        numberOfEnergyUnits: _numberOfEnergyUnits,
-        price: _price
-    });
-
-    buyOffers.push(buyOffer);
-
-    // Update the corresponding SellOffer's buyOfferIds and bestBuyOfferId
-    sellOffer.buyOfferIds.push(newBuyOfferId);
-
-    if (sellOffer.bestBuyOfferId == 0 || ( _price * _numberOfEnergyUnits < buyOffers[sellOffer.bestBuyOfferId].price * buyOffers[sellOffer.bestBuyOfferId].numberOfEnergyUnits)) {
-        sellOffer.bestBuyOfferId = newBuyOfferId;
     }
 
-    // Add the buy offer ID to the buyer's buyOfferList
-    buyOfferList[msg.sender].push(newBuyOfferId);
-  }
+    function createBuyOffer(uint _sellOfferId, uint _numberOfEnergyUnits, uint _price) public sellOfferExists(_sellOfferId) {
+        SellOffer storage sellOffer = sellOffers[_sellOfferId];
 
+        require(msg.sender != sellOffer.seller, "Buyer cannot be the seller");
+        require(_numberOfEnergyUnits > 0, "Number of energy units must be greater than 0");
+        require(_price > 0, "Price must be greater than 0");
 
-  function completeTransaction(uint _sellOfferId) public sellOfferExists(_sellOfferId) {
-    // Retrieve the relevant sell offer
-    SellOffer storage sellOffer = sellOffers[_sellOfferId];
+        uint newBuyOfferId = newBuyOfferId;
+        BuyOffer memory buyOffer = BuyOffer({
+            id: newBuyOfferId,
+            buyer: payable(msg.sender),
+            sellOfferId: _sellOfferId,
+            numberOfEnergyUnits: _numberOfEnergyUnits,
+            price: _price
+        });
 
-    require(!sellOffer.sold, "Sell offer has already been sold");
-    require(sellOffer.seller == msg.sender, "Only the seller can complete the transaction");
+        buyOffers[newBuyOfferId] = buyOffer;
 
-    // Get the buy offer ID from the bestBuyOfferId
-    uint buyOfferId = sellOffer.bestBuyOfferId;
+        sellOffer.buyOfferIds.push(newBuyOfferId);
 
-    // Ensure that there is a valid buy offer
-    require(buyOfferId > 0, "No valid buy offer found");
+        if (sellOffer.bestBuyOfferId == 0 || (_price * _numberOfEnergyUnits < buyOffers[sellOffer.bestBuyOfferId].price * buyOffers[sellOffer.bestBuyOfferId].numberOfEnergyUnits)) {
+            sellOffer.bestBuyOfferId = newBuyOfferId;
+        }
 
-    // Retrieve the relevant buy offer
-    BuyOffer storage buyOffer = buyOffers[buyOfferId];
+        // Add the buy offer ID to the buyer's buyOfferList
+        buyOfferList[msg.sender].push(newBuyOfferId);
+    }
 
-    // Check if the number of energy units being purchased is within the valid range
-    require(
-        buyOffer.numberOfEnergyUnits >= sellOffer.minEnergyUnits && 
-        buyOffer.numberOfEnergyUnits <= sellOffer.maxEnergyUnits,
-        "Invalid number of energy units"
-    );
+    function completeTransaction(uint _sellOfferId) public payable sellOfferExists(_sellOfferId) {
+        SellOffer storage sellOffer = sellOffers[_sellOfferId];
 
-    // Calculate the total cost of the transaction
-    uint totalCost = buyOffer.numberOfEnergyUnits * buyOffer.price;
+        require(!sellOffer.sold, "Sell offer has already been sold");
+        require(sellOffer.seller == msg.sender, "Only the seller can complete the transaction");
 
-    // Ensure the contract has sufficient balance to cover the transaction
-    require(address(this).balance >= totalCost, "Insufficient contract balance");
+        uint buyOfferId = sellOffer.bestBuyOfferId;
 
-    // Transfer the energy units to the buyer
-    sellOffer.seller.transfer(buyOffer.numberOfEnergyUnits);
+        require(buyOfferId > 0, "No valid buy offer found");
 
-    // Transfer the payment to the seller
-    buyOffer.buyer.transfer(totalCost);
+        BuyOffer storage buyOffer = buyOffers[buyOfferId];
 
-    // Mark the sell offer as sold
-    sellOffer.sold = true;
+        require(
+            buyOffer.numberOfEnergyUnits >= sellOffer.minEnergyUnits &&
+            buyOffer.numberOfEnergyUnits <= sellOffer.maxEnergyUnits,
+            "Invalid number of energy units"
+        );
 
-    // Remove the completed buy offer from the buyer's buyOfferList
-    removeBuyOffer(buyOfferId);
-  }
+        uint totalCost = buyOffer.numberOfEnergyUnits * buyOffer.price;
 
-  function removeBuyOffer(uint _buyOfferId) private {
+        if (buyOffer.buyer.send(totalCost)) {
+          // Mark the sell offer as sold
+          sellOffer.sold = true;
+
+        } else {
+          // If the payment transfer fails, revert the transaction and remove the buy offer from the sell offer list
+          revert("Payment transfer failed"); 
+        }
+        // Remove the completed buy offer from the buyer's buyOfferList
+        removeBuyOffer(buyOfferId);
+    }
+
+    function removeBuyOffer(uint _buyOfferId) private {
     BuyOffer storage buyOffer = buyOffers[_buyOfferId];
     
     // Find the index of the buy offer in the buyer's buyOfferList
@@ -153,48 +138,64 @@ contract EnergySharing {
     
     buyOfferList[buyOffer.buyer][indexToDelete] = lastBuyOfferId;
     buyOfferList[buyOffer.buyer].pop();
-  }
-  
-  function getAllCurrentSellOffers() public view returns (SellOffer[] memory) {
-    SellOffer[] memory currentSellOffers = new SellOffer[](sellOffers.length);
-    uint currentSellOfferCount = 0;
 
-    for (uint i = 1; i < sellOffers.length; i++) {
-        // Skip the first sell offer with ID 0 (non-existent)
-        if (!sellOffers[i].sold) {
-            currentSellOffers[currentSellOfferCount] = sellOffers[i];
-            currentSellOfferCount++;
-        }
-    }
+    // Remove the buy offer ID from the sell offer's buyOfferIds
+    uint sellOfferId = buyOffer.sellOfferId;
+    uint[] storage sellOfferBuyOfferIds = sellOffers[sellOfferId].buyOfferIds;
+    uint sellOfferIndexToDelete = findIndex(sellOfferBuyOfferIds, _buyOfferId);
 
-    // Resize the array to exclude any unused slots
-    assembly {
-        mstore(currentSellOffers, currentSellOfferCount)
-    }
+    // Ensure the buy offer is found in the sell offer's list
+    require(sellOfferIndexToDelete < sellOfferBuyOfferIds.length, "Buy offer not found in the sell offer's list");
 
-    return currentSellOffers;
-  }
-
-  function getUserSellOffers(address _user) public view returns (SellOffer[] memory) {
-    uint[] storage userSellOfferIds = sellOfferList[_user];
-    SellOffer[] memory userSellOffers = new SellOffer[](userSellOfferIds.length);
-
-    for (uint i = 0; i < userSellOfferIds.length; i++) {
-        userSellOffers[i] = sellOffers[userSellOfferIds[i]];
-    }
-
-    return userSellOffers;
-  }
-
-  function getUserBuyOffers(address _user) public view returns (BuyOffer[] memory) {
-    uint[] storage userBuyOfferIds = buyOfferList[_user];
-    BuyOffer[] memory userBuyOffers = new BuyOffer[](userBuyOfferIds.length);
-
-    for (uint i = 0; i < userBuyOfferIds.length; i++) {
-        userBuyOffers[i] = buyOffers[userBuyOfferIds[i]];
-    }
-
-    return userBuyOffers;
-  }
-
+    // Swap the buy offer to be deleted with the last one and then remove the last element
+    uint sellOfferLastIndex = sellOfferBuyOfferIds.length - 1;
+    uint sellOfferLastBuyOfferId = sellOfferBuyOfferIds[sellOfferLastIndex];
+    
+    sellOfferBuyOfferIds[sellOfferIndexToDelete] = sellOfferLastBuyOfferId;
+    sellOfferBuyOfferIds.pop();
 }
+
+
+    function findIndex(uint[] storage arr, uint value) internal view returns (uint) {
+        for (uint i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return type(uint).max;
+    }
+
+    function getAllCurrentSellOffers() public view returns (SellOffer[] memory) {
+        SellOffer[] memory currentSellOffers = new SellOffer[](sellOfferKeys.length);
+
+        for (uint i = 0; i < sellOfferKeys.length; i++) {
+            uint sellOfferId = sellOfferKeys[i];
+            currentSellOffers[i] = sellOffers[sellOfferId];
+        }
+
+        return currentSellOffers;
+    }
+
+    function getUserSellOffers(address _user) public view returns (SellOffer[] memory) {
+        uint[] storage userSellOfferIds = sellOfferList[_user];
+        SellOffer[] memory userSellOffers = new SellOffer[](userSellOfferIds.length);
+
+        for (uint i = 0; i < userSellOfferIds.length; i++) {
+            userSellOffers[i] = sellOffers[userSellOfferIds[i]];
+        }
+
+        return userSellOffers;
+    }
+
+    function getUserBuyOffers(address _user) public view returns (BuyOffer[] memory) {
+        uint[] storage userBuyOfferIds = buyOfferList[_user];
+        BuyOffer[] memory userBuyOffers = new BuyOffer[](userBuyOfferIds.length);
+
+        for (uint i = 0; i < userBuyOfferIds.length; i++) {
+            userBuyOffers[i] = buyOffers[userBuyOfferIds[i]];
+        }
+
+        return userBuyOffers;
+    }
+}
+
--- scripts/interact.js
+++ scripts/interact.js
@@ -5,46 +5,4 @@ const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const { ethers } = require("hardhat");
 const contract = require("../artifacts/contracts/energyshare.sol/EnergySharing.json");
 
-// uncomment the line below to see the ABI in the console
-// console.log(JSON.stringify(contract.abi));
-
-const alchemyProvider = new ethers.providers.AlchemyProvider(
-    (network = "goerli"),
-    API_KEY
-);
-
-const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
-
-const contractInstance = new ethers.Contract(
-    CONTRACT_ADDRESS,
-    contract.abi,
-    signer,
-)
-
-// function: create a new energy transaction
-async function createEnergyTransaction(_seller, _buyer, _energyAmount, _price) {
-    try {
-        const transaction = await contractInstance.createTransaction(_seller, _buyer, _energyAmount, _price, {
-            value: ethers.utils.parseEther((_energyAmount * _price).toString())
-        });
-        await transaction.wait();
-        console.log("Transaction Hash:", transaction.hash);
-    } catch (error) {
-        console.error("Error:", error);
-    }
-
-}// function: complete an energy transaction
-async function completeEnergyTransaction(transactionId) {
-    try {
-        const transaction = await contractInstance.completeTransaction(_transactionId);
-        await transaction.wait();
-        console.log("Transaction Hash:", transaction.hash);
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
-
-module.exports = {
-    createEnergyTransaction,
-    completeEnergyTransaction,
-};
+console.log(JSON.stringify(contract.abi));
